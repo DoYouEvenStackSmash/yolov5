7842003aff9fd863fedae9920723f736d5a98681
Jake <manumoomaw@gmail.com>
2023-05-31 04:28:14 -0700

    hotfix for center of bounding box

--- detect_support.py
+++ detect_support.py
@@ -157,8 +157,8 @@ def create_detection_without_range(sensing_agent, sensor_origin, time_of_detecti
     """
     Creates a detection when range is not present
     """
-
-    rel_x = (x * img_shape_x + (w * img_shape_x) / 2 - (img_shape_x / 2)) / img_shape_x * 100 + 50
+    #(w * img_shape_x) / 2
+    rel_x = (x * img_shape_x - (img_shape_x / 2)) / img_shape_x * 100 + 50
     print(f"x val: {x}")
     rel_y = 100
     bbox = [rel_x, rel_y, w, h]
