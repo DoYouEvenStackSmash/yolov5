6f76d8d7d1cedf40bb5d496fcc4859254e9f21b6
Jake <manumoomaw@gmail.com>
2023-05-30 21:14:06 -0700

    hotfix for local detection coord

--- detect_support.py
+++ detect_support.py
@@ -162,10 +162,12 @@ def create_detection_without_range(sensing_agent, sensor_origin, time_of_detecti
 
     fixed_range = 100
     range_to_target = fixed_range
-    theta = mfn.euclidean_dist((center_x,0), (det_center_x,0)) / img_shape_x * sensor_fov_width
+    rel_y = (det_center_x / img_shape_x) * 100 - 100
+    # theta = mfn.euclidean_dist((center_x,0), (det_center_x,0)) / img_shape_x * sensor_fov_width
 
-    detection_coord = mfn.pol2car(sensor_origin, range_to_target, theta)
-    posn = Position(detection_coord[0], detection_coord[1])
+    # detection_coord = mfn.pol2car(sensor_origin, range_to_target, theta)
+    detection_coord = (100, rel_y)
+    posn = Position(100, rel_y)
     
     sensor_coord = sensing_agent.transform_to_local_sensor_coord((0,0), detection_coord)
 
